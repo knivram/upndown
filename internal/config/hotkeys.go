d27c4148e4675ac60a5903cbbc76735bd03e022f
knivram <129607867+knivram@users.noreply.github.com>
2025-07-10 10:05:19 +0200

    changed the code to just go to the position ether up or down

--- internal/config/hotkeys.go
+++ internal/config/hotkeys.go
@@ -1,6 +1,9 @@
 package config
 
-import "golang.design/x/hotkey"
+import (
+	"github.com/knivram/upndown/internal/tinkerforge"
+	"golang.design/x/hotkey"
+)
 
 type HotkeyConfig struct {
 	Modifiers []hotkey.Modifier
@@ -10,8 +13,7 @@ type HotkeyConfig struct {
 }
 
 type TinkerforgeClient interface {
-	Up() uint64
-	Down() uint64
+	GoTo(position uint16) uint64
 }
 
 func GetHotkeyConfig(client TinkerforgeClient) []HotkeyConfig {
@@ -19,13 +21,13 @@ func GetHotkeyConfig(client TinkerforgeClient) []HotkeyConfig {
 		{
 			Modifiers: []hotkey.Modifier{hotkey.ModShift, hotkey.ModCmd},
 			Key:       hotkey.KeyF11,
-			Action:    func() { client.Up() },
+			Action:    func() { client.GoTo(tinkerforge.PositionUp) },
 			Desc:      "Up",
 		},
 		{
 			Modifiers: []hotkey.Modifier{hotkey.ModShift, hotkey.ModCmd},
 			Key:       hotkey.KeyF12,
-			Action:    func() { client.Down() },
+			Action:    func() { client.GoTo(tinkerforge.PositionDown) },
 			Desc:      "Down",
 		},
 	}
--- internal/tinkerforge/actions.go
+++ internal/tinkerforge/actions.go
@@ -8,17 +8,34 @@ import (
 
 const (
 	// Hight in millimeters
-	PositionUp   = 1060
+	PositionUp   = 1095
 	PositionDown = 670
 )
 
-func (c *Client) Up() uint64 {
-	log.Println("Starting up...")
+func (c *Client) GoTo(position uint16) uint64 {
+	currentPosition, err := c.distanceIR.GetDistance()
+	if err != nil {
+		log.Println("Error getting current position:", err)
+		return 0
+	}
+	log.Printf("Current position: %d, target position: %d", currentPosition, position)
+	if currentPosition < position {
+		return c.moveUpTo(position)
+	}
+	if currentPosition > position {
+		return c.moveDownTo(position)
+	}
+	log.Println("Current position is the same as the target position")
+	return 0
+}
+
+func (c *Client) moveUpTo(position uint16) uint64 {
+	log.Printf("Starting up to %d...", position)
 	c.industrialDualRelay.SetValue(false, true)
 	c.distanceIR.SetDistanceCallbackConfiguration(10, true, distance_ir_v2_bricklet.ThresholdOptionOff, 0, 0)
 	var callbackID uint64
 	callbackID = c.distanceIR.RegisterDistanceCallback(func(distance uint16) {
-		if distance > PositionUp {
+		if distance > position {
 			c.industrialDualRelay.SetValue(true, true)
 			log.Println("CallbackID", callbackID)
 			c.distanceIR.DeregisterDistanceCallback(callbackID)
@@ -29,13 +46,13 @@ func (c *Client) Up() uint64 {
 	return callbackID
 }
 
-func (c *Client) Down() uint64 {
-	log.Println("Starting down...")
+func (c *Client) moveDownTo(position uint16) uint64 {
+	log.Printf("Starting down to %d...", position)
 	c.industrialDualRelay.SetValue(true, false)
 	c.distanceIR.SetDistanceCallbackConfiguration(10, true, distance_ir_v2_bricklet.ThresholdOptionOff, 0, 0)
 	var callbackID uint64
 	callbackID = c.distanceIR.RegisterDistanceCallback(func(distance uint16) {
-		if distance < PositionDown {
+		if distance < position {
 			c.industrialDualRelay.SetValue(true, true)
 			log.Println("CallbackID", callbackID)
 			c.distanceIR.DeregisterDistanceCallback(callbackID)
